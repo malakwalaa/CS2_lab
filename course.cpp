0a82bb284183e9278a015a9b6c97c1b312ab33a1
malakwalaa <malakwalaa2004@aucegypt.edu>
2024-11-24 14:05:18 +0200

    adjusted course and events class and header

--- course.cpp
+++ course.cpp
@@ -1,3 +1,22 @@
 #include "course.h"
 
-course::course() {}
+// Constructor
+course::course(const string& courseId, const string& name, const string& instructor, const string& schedule)
+    : course_id(course_id), name(name), professor(professor), schedule(schedule) {}
+
+// Getters
+string course::get_courseid() const {
+    return course_id;
+}
+
+string course::get_name() const {
+    return name;
+}
+
+string course::get_prof() const {
+    return professor;
+}
+
+string course::get_schedule() const {
+    return schedule;
+}
--- event.cpp
+++ event.cpp
@@ -1,3 +1,21 @@
 #include "event.h"
 
-event::event() {}
+event::event(const std::string& eventId, const std::string& title, const std::string& speaker, const std::string& schedule)
+    : eventid(eventId), title(title), speaker(speaker), schedule(schedule) {}
+
+// Getters
+string event::get_event_idEventId() const {
+    return eventid;
+}
+
+string event::get_title() const {
+    return title;
+}
+
+string event::get_speaker() const {
+    return speaker;
+}
+
+string event::get_schedule() const {
+    return schedule;
+}
