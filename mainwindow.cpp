aab41b70d90f03677f395237704579bc26b25948
malakwalaa <malakwalaa2004@aucegypt.edu>
2024-11-25 14:24:56 +0200

    same as before

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,12 +19,10 @@ void MainWindow::on_student_push_button_clicked()
 {
     if (!studentLogin) { // Create the StudentLogin dialog if not already created
         studentLogin = new StudentLogin(this);
-        connect(studentLogin, &StudentLogin::destroyed, [this]() {
-            this->show(); // Show MainWindow when StudentLogin is closed
-        });
     }
-    studentLogin->show(); // Show the StudentLogin window
     this->hide();         // Hide the MainWindow
+    studentLogin->exec(); // Show the dialog modally
+    this->show();         // Show MainWindow after dialog closes
 
 }
 
