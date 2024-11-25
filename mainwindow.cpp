f41771bb6abfca5a55d85d12dd8f50b0bb333590
malakwalaa <malakwalaa2004@aucegypt.edu>
2024-11-25 14:22:48 +0200

    talia, sarah and i tried to fix the logic error in student button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,8 +19,12 @@ void MainWindow::on_student_push_button_clicked()
 {
     if (!studentLogin) { // Create the StudentLogin dialog if not already created
         studentLogin = new StudentLogin(this);
+        connect(studentLogin, &StudentLogin::destroyed, [this]() {
+            this->show(); // Show MainWindow when StudentLogin is closed
+        });
     }
-    studentLogin->show();
-    this->hide();
+    studentLogin->show(); // Show the StudentLogin window
+    this->hide();         // Hide the MainWindow
+
 }
 
