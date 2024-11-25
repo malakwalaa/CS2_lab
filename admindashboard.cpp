d1372dd5e45f3500e8c077b8aec49cd52fcbaaf8
saaarahh <sarahssoliman@aucegypt.edu>
2024-11-25 15:54:13 +0200

    Update admindashboard.cpp

--- admindashboard.cpp
+++ admindashboard.cpp
@@ -1,14 +1,31 @@
 #include "admindashboard.h"
 #include "ui_admindashboard.h"
-
+#include "coursemanagement.h"
 admindashboard::admindashboard(QWidget *parent)
     : QDialog(parent)
-    , ui(new Ui::admindashboard)
+    , ui(new Ui::admindashboard),
+ courseManagement(nullptr)
 {
     ui->setupUi(this);
 }
 
+
 admindashboard::~admindashboard()
 {
     delete ui;
+    if (courseManagement) {
+        delete courseManagement; // Clean up if created
+    }
 }
+
+void admindashboard::on_pushButton_clicked()
+{
+    if (!courseManagement) {
+        courseManagement = new coursemanagement(this); // Create it with this as parent
+    }
+
+    courseManagement->show();      // Show the CourseManagement dialog
+    courseManagement->raise();     // Bring it to the front
+    courseManagement->activateWindow(); // Give it focus
+}
+
